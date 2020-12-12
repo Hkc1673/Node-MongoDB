5d5f2d8ecbd78c5e35e8c1da02572de6e36b4f03
Hkc-1673 <habibkc71@gmail.com>
2020-12-12 17:56:12 +0200

    Add user register email check

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,17 +1,33 @@
-exports.authRegister = (req, res) => {
+const User = require("../models/UserModel");
+const bcrypt = require("bcryptjs");
+
+exports.authRegister = async (req, res) => {
   const { firstName, lastName, email, password } = req.body;
-  console.log(
-    "Fields:",
+
+  // TODO1: Validate the fields
+
+  const userData = await User.findOne({ email });
+
+  if (userData) {
+    return res
+      .status(400)
+      .json({ errors: [{ message: "User already exists!!" }] });
+  }
+
+  const salt = await bcrypt.genSalt(10);
+
+  const newPassword = await bcrypt.hash(password, salt);
+
+  const user = new User({
     firstName,
     lastName,
     email,
-    password
-  );
-  // TODO1: Validate the fields
-  // TODO2: check already registered
-  // TODO3: crpyt password
-  // TODO4: save the user to DB
+    password: newPassword,
+  });
 
+  await user.save();
+
+  //TODO: Error handling for saving
   res.send("Register Completed.");
 };
 
