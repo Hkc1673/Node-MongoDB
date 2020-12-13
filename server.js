02f67541b7194de10020e5a844b5869272c5677f
Hkc-1673 <habibkc71@gmail.com>
2020-12-13 18:47:12 +0200

    Add prodcution case

--- server.js
+++ server.js
@@ -11,6 +11,15 @@ connectDB();
 app.use(express.json());
 app.use("/api", router);
 
-app.listen(process.env.PORT, () => {
-  console.log(`I'm listening on port ${process.env.PORT}`);
+// production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`I'm listening on port ${port}`);
 });
